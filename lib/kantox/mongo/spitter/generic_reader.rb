a7e23ebeb88a6ff52a0093ec536a091147383086
Aleksei Matiushkin <aleksei.matiushkin@kantox.com>
2015-08-05 17:07:38 +0200

    Added generic reader.

---
+++ lib/kantox/mongo/spitter/generic_reader.rb
@@ -0,0 +1,13 @@
+module Kantox
+  module Mongo
+    module Spitter
+      class GenericCollectionItem
+        include MongoMapper::Document
+      end
+
+      class GenericReader
+#        MongoMapper.database = 
+      end
+    end
+  end
+end
